642cd74220e549ecf406354c5332fe6d520656cc
vjtuomin <valtteri.j.tuominen@student.jyu.fi>
2022-02-23 19:37:37 +0200

    part4

---
+++ part4/App.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+
+
+mongoose.connect(config.MONGODB_URI)
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports=app
+
+
+
